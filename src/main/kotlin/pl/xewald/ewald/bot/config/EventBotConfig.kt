68fb82abc2ce165b373267cdee16047a12efb323
Ewald <kubagloger@gmail.com>
2018-03-19 17:27:56 +0100

    Drobne poprawki

--- src/main/kotlin/pl/xewald/ewald/bot/config/EventBotConfig.kt
+++ src/main/kotlin/pl/xewald/ewald/bot/config/EventBotConfig.kt
@@ -3,7 +3,7 @@ package pl.xewald.ewald.bot.config
 class EventBotConfig {
 
     @FieldName("token")
-    var token = "NDIyODUxNTEzMzE3MzI2ODc4.DZBCWg.bRgcCQAKX8EOY22guSAli2eezrY"
+    var token = "TOKEN"
     private set
 
 }
