ec75a8f8e6dbab06dafa8447facece0358e872f6
Ewald <kubagloger@gmail.com>
2018-03-19 19:16:17 +0100

    .
    .

---
+++ src/main/kotlin/pl/xewald/ewald/bot/config/EwaldBotConfig.kt
@@ -0,0 +1,9 @@
+package pl.xewald.ewald.bot.config
+
+class EwaldBotConfig {
+
+    @FieldName("token")
+    var token = "NDIyODUxNTEzMzE3MzI2ODc4.DZGATw.4GNqtb3DsWDFlhsA4is5QI8wMwg"
+    private set
+
+}
