7ff277a2ddd7495aa42f45960d6af59ccec8b1b5
Ixidi <xlinux3@gmail.com>
2018-04-15 20:16:58 +0200

    Add sorting of commands by categories in help command.

--- src/main/kotlin/pl/xewald/ewald/bot/command/bot/HelpCommand.kt
+++ src/main/kotlin/pl/xewald/ewald/bot/command/bot/HelpCommand.kt
@@ -15,11 +15,19 @@ class HelpCommand(val bot: EwaldBot) : Command(
     override fun execute(member: Member?, channel: MessageChannel, message: Message, args: Array<String>) {
         val commandMap = bot.commandManager.commandMap
         val embed = basicEmbedBuilder("Pomoc")
-        val builder = StringBuilder()
-        for ((name, command) in commandMap) {
-            builder.append("`!$name - ${command.description}`\n")
+        val categoryMap = HashMap<CommandCategory, List<Command>>()
+        CommandCategory.values().forEach { category ->
+            val list = ArrayList<Command>()
+            commandMap.values.stream()
+                    .filter { command -> command.category == category }
+                    .forEach { command -> list.add(command) }
+            categoryMap[category] = list
         }
-        embed.setDescription(builder.toString())
+        for ((category, list) in categoryMap) {
+            val builder = StringBuilder()
+            list.forEach { command ->  builder.append("`!${command.name} - ${command.description}`\n") }
+            embed.addField(category.name.toLowerCase().capitalize(), builder.toString(), false)
+        }
         embed.setColor(member!!.color)
         channel.sendMessage(embed.build()).queue()
     }
